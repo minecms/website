3e52f5ec6273b305a5a757b1bfc68acd478048e4
Fubon - Frontend Development <43171309+thefubon@users.noreply.github.com>
2026-05-15 19:03:23 +0300

    upd

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "@/styles/globals.css";
 import { cn } from "@/lib/utils";
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
-  title: "Fubon",
-  description: "",
-};
+  title: 'MineCMS',
+  description: '',
+}
 
 export default function RootLayout({
   children,
@@ -28,7 +15,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
+      className={cn("h-full", "antialiased")}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,9 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/fubon.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className=" text-3xl font-bold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Design &<br />
-            Frontend Development
+            MineCMS
           </h1>
         </div>
       </main>
